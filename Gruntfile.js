a3bb94e007f0be40fbcc33ec7ab6531eb314c6c5
forthright48 <forthright48@gmail.com>
2015-12-08 13:09:53 +0600

    Added grunt wiredep, comments to bind bower css and js dependencies and change default directory of bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,14 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+
+        wiredep: {
+            target: {
+                src: 'public/home.html' // point to your HTML file.
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-wiredep');
 };
